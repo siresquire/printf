70aa546a3bcc4b65aa5a0e2e120dcacd9da2827d
T-LES <lucaselat@gmail.com>
2022-07-12 17:26:22 +0000

    Updating file

--- print_long_unt.c
+++ print_long_unt.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * printlunt - prints a long unsigned integer
- * @l: number to print
- * @b: buffer pointer
- * @ib: index for buffer pointer
+ * prinlunt - prints a long unsigned integer
+ * @arguments: list of arguments
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-int printlunt(va_list l, char *b, unsigned int ib)
+int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
 {
 	unsigned long int i, j, k, d;
 
-	i = va_arg(l, unsigned long int);
+	i = va_arg(arguments, unsigned long int);
 	j = i;
 	d = 1;
 	while (j > 9)
@@ -19,6 +19,6 @@ int printlunt(va_list l, char *b, unsigned int ib)
 		j /= 10;
 	}
 	for (k = 0; d > 0; d /= 10, k++)
-		ib = handl_buf(b, ((i / d) % 10) + '0', ib);
+		ibuf = handl_buf(buf, ((i / d) % 10) + '0', ibuf);
 	return (k);
 }
