ad54ebcb06cd1d9c1de7e3d433f189a60da776f9
T-LES <lucaselat@gmail.com>
2022-07-12 17:23:24 +0000

    Updating file

--- print_int.c
+++ print_int.c
@@ -2,22 +2,22 @@
 
 /**
  * print_int - prints an integer
- * @l: list of argument
- * @b: buffer pointer
- * @ib: index for buffer pointer
+ * @arguments: list of argument
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  * Return: number of char printed
  */
-int print_int(va_list l, char *b, unsigned int ib)
+int print_int(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int var_input;
 	unsigned int i, j, k, d, g;
 
-	var_input = va_arg(l, int);
+	var_input = va_arg(arguments, int);
 	g = 0;
 	if (var_input < 0)
 	{
 		i = var_input * -1;
-		ib = handl_buf(buf, '-', ib);
+		ibuf = handl_buf(buf, '-', ibuf);
 		g = 1;
 	}
 	else
@@ -30,6 +30,6 @@ int print_int(va_list l, char *b, unsigned int ib)
 		j /= 10;
 	}
 	for (k = 0; d > 0; d /= 10, k++)
-		ib = handl_buf(b, ((i / d) % 10) + '0', ib);
+		ibuf = handl_buf(buf, ((i / d) % 10) + '0', ibuf);
 	return (k + g);
 }
