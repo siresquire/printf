725066f0b9cb8a60f26fa7dce631e7872ba95a27
T-LES <lucaselat@gmail.com>
2022-07-12 16:52:10 +0000

    Updating file

--- print_str.c
+++ print_str.c
@@ -1,26 +1,26 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_str - writes the string to stdout
- * @l: list of arguments
- * @b: buffer pointer
- * @ib: index for buffer pointer
+ * @arguments: list of arguments
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  * Return: On success 1
  */
-int print_str(va_list l, char *b, unsigned int ib)
+int print_str(va_list arguments, char *buf, unsigned int ibuf)
 {
 	char *s;
 	unsigned int i;
 	char n[] = "(null)";
 
-	s = va_arg(l, char *);
+	s = va_arg(arguments, char *);
 	if (s == NULL)
 	{
 		for (i = 0; n[i]; i++)
-			ib = handl_buf(b, n[i], ib);
+			ibuf = handl_buf(buf, n[i], ibuf);
 		return (6);
 	}
 	for (i = 0; str[i]; i++)
-		ib = handl_buf(b, s[i], ib);
+		ibuf = handl_buf(buf, s[i], ibuf);
 	return (i);
 }
