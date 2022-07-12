e14409ec3f9d3f800aac43d74de8a9823ee2512e
T-LES <lucaselat@gmail.com>
2022-07-12 16:57:40 +0000

    Updating file

--- print_add.c
+++ print_add.c
@@ -3,23 +3,23 @@
 
 /**
  * print_add - prints the qdress of an input variable
- * @l: input address
- * @b: buffer pointer
- * @ib: index for buffer pointer
+ * @arguments: list of arguments
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  * Return: number of char printed
  */
-int print_add(va_list l, char *b, unsigned int *ib)
+int print_add(va_list arguments, char *buf, unsigned int *ibuf)
 {
 	void *d;
 	long int var_input;
 	int i, j, fd, g;
 	char *hexa, *bin, n[] = "(nil)";
 
-	d = (va_arg(l, void *));
+	d = (va_arg(arguments, void *));
 	if (d == NULL)
 	{
 		for (i = 0; n[i]; i++)
-			ib = handl_buf(buf, n[i], ib);
+			ibuf = handl_buf(buf, n[i], ibuf);
 		return (5);
 	}
 	var_input = (intprt_t)d;
@@ -33,16 +33,16 @@ int print_add(va_list l, char *b, unsigned int *ib)
 	bin = fill_binary_array(bin, var_input, g, 64);
 	hexa = malloc(sizeof(char) * (16 + 1));
 	hexa = fill_hex_array(bin, hexa, 0, 16);
-	ib = handl_buf(b, '0', ib);
-	ib = handl_buf(b, 'x', ib);
+	ibuf = handl_buf(buf, '0', ibuf);
+	ibuf = handl_buf(buf, 'x', ibuf);
 	for (fd = i = j = 0; hexa[i]; i++)
 	{
 		if (hexa[i] != '0' && fd == 0)
 			fd = 1;
 		if (fd)
 		{
-			ib = handl_buf(b, hexa[i], ib);
-			j;
+			ibuf = handl_buf(buf, hexa[i], ibuf);
+			j++;
 		}
 	}
 	free(bin);
