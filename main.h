30a22ccb369f90ad60c2f664656eda1eeef1f3a7
T-LES <lucaselat@gmail.com>
2022-07-12 07:58:04 +0000

    How to print an integer

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ int print_bnr(va_list l, char *b, unsigned int ib);
 int print_buf(char *b, unsigned int n);
 int print_chr(va_list l, char b, unsigned int i);
 int print_hex(va_list l, char *b, unsigned int ib);
+int print_int(va_list l, char *b, unsigned int ib);
 unsigned int handl_buf(char *b, char c, unsigned int ib);
 int print_buf(char *buf, unsigned int nbuf);
 char *fill_binary_array(char *bin, long int int_in, int isneg, int limit);
---
+++ print_int.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_int - prints an integer
+ * l: list of argument
+ * @b: buffer pointer
+ * @ib: index for buffer pointer
+ * Return: number of char printed
+ */
+int print_int(va_list l, char *b, unsigned int ib)
+{
+	int var_input;
+	unsigned int i, j, k, d, g;
+	var_input = va_arg(l, int);
+	g = 0;
+	if (var_input < 0)
+	{
+		i = var_input * -1;
+		ib = handl_buf(buf, '-', ib);
+		g = 1;
+	}
+	else
+		i = var_input;
+	j = i;
+	d = 1;
+	while (j > 9)
+	{
+		d *= 10;
+		j /= 10;
+	}
+	for (k = 0; d > 0; d /= 10, k++)
+		ib = handl_buf(b, ((i / d) % 10) + '0', ib);
+	return (k + g);
+}
