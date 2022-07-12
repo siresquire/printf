ad7e4cd9833e360e725a70f664b121307ca287ae
T-LES <lucaselat@gmail.com>
2022-07-12 13:16:59 +0000

    prints a percent symbol

---
+++ print_prg.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_prg - writes the character c to stdout
+ * @a: input char
+ * @b: buffer pointer
+ * @ib: index for buffer pointer
+ * Return: On success 1
+ */
+int print_prg(va_list a __attribute__((unused)), char *b, unsigned int ib)
+{
+	handl_buf(b, '%', ib);
+	return (1);
+}
