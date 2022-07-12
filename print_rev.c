74e11ad8cf0fc30785c1870d9e6eb07aee305ab3
T-LES <lucaselat@gmail.com>
2022-07-12 17:33:40 +0000

    Updating file

--- print_rev.c
+++ print_rev.c
@@ -2,29 +2,29 @@
 
 /**
  * print_rev - writes the string s in reverse
- * @l: list of arguments
- * @b: buffer pointer
- * @ib: index for buffer pointer
+ * @arguments: list of arguments
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
  * Return: number of char printed.
  */
-int print_rev(va_list l, char *b, unsigned int ib)
+int print_rev(va_list arguments, char *buf, unsigned int ibuf)
 {
 	char *s;
 	unsigned int i;
 	int j = 0;
 	char n[] = "(llun)";
 
-	s = va_arg(l, char *);
+	s = va_arg(arguments, char *);
 	if (s == NULL)
 	{
 		for (i = 0; n[i]; i++)
-			ib = handl_buf(b, n[i], ib);
+			ibuf = handl_buf(buf, n[i], ibuf);
 		return (6);
 	}
 	for (i = 0; s[i]; i++)
 		;
 	j = i - 1;
 	for (; j >= 0; j--)
-		ib = handl_buf(b, s[j], ib);
+		ibuf = handl_buf(buf, s[j], ibuf);
 	return (i);
 }
